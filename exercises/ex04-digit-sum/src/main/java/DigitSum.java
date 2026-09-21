07d67a1d449f5eede119c4a6347c4899137c0910
Lei Zhang <leiz123321@gmail.com>
2026-09-21 09:01:20 -0400

    exercise 3 and 4

--- exercises/ex04-digit-sum/src/main/java/DigitSum.java
+++ exercises/ex04-digit-sum/src/main/java/DigitSum.java
@@ -21,7 +21,11 @@ public class DigitSum {
      * @return the sum of its decimal digits
      */
     public static int digitSum(int n) {
-        // TODO: complete
-        return 0;
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
