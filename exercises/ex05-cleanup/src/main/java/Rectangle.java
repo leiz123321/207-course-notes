32902741eeebd395629dc6231798ca91b00a7469
Lei Zhang <leiz123321@gmail.com>
2026-09-22 15:36:35 -0400

    exercise 6

--- exercises/ex05-cleanup/src/main/java/Rectangle.java
+++ exercises/ex05-cleanup/src/main/java/Rectangle.java
@@ -1,8 +1,9 @@
 public class Rectangle {
+
     private double width;
     private double height;
 
-    public Rectangle(double width,double height) {
+    public Rectangle(double width, double height) {
         this.width = width;
         this.height = height;
     }
